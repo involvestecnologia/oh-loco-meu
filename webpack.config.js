93fbc26698ed142183ab9dd6158b986c7a2adb83
Thiago Zanetti <thiago.project@gmail.com>
2017-08-01 14:38:56 -0300

    Adding babel.

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
@@ -6,6 +7,23 @@ module.exports = {
     filename: 'ohlocomeu.bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  module: {
+  rules: [
+    {
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['env']
+        }
+      }
+    }
+  ]
+},
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin(),
+  ],
   node: {
     fs: 'empty',
   }
