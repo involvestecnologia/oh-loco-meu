41ecbbb3b20eb50025f7f2145caab7992be9ce39
Thiago Zanetti <thiago.project@gmail.com>
2017-08-02 15:30:34 -0300

    Adding axios to externals.

--- webpack.config.js
+++ webpack.config.js
@@ -11,24 +11,29 @@ module.exports = {
   },
   target: 'node',
   module: {
-  rules: [
-    {
-      test: /\.js$/,
-      exclude: /(node_modules|bower_components)/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['env']
-        }
-      }
-    }
-  ]
-},
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env'],
+          },
+        },
+      },
+    ],
+  },
   plugins: [
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin(),
   ],
+  externals : {
+    axios: {
+      commonjs: "axios",
+    },
+  },
   node: {
     fs: 'empty',
-  }
+  },
 };
