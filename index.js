af589b741826a7dc6a45b25ca845f7a99e8f861e
Neri Bez Fontana <nerifontana@gmail.com>
2019-04-12 18:17:27 -0300

    Promisify main functions
    Change get and getAll functions to return a promise so we can use oh-loco-meu with gulp 4

--- index.js
+++ index.js
@@ -19,20 +19,20 @@ const writeFile = result => {
   JSON.stringify(result.data) :
   result.data;
 
-  if (!fs.existsSync(result.props.pathPrefix)) {
-    fs.mkdirSync(result.props.pathPrefix);
+  if (!fs.existsSync(result.localeData.pathPrefix)) {
+    fs.mkdirSync(result.localeData.pathPrefix);
   }
 
-  fs.writeFile(result.props.filePath, fileContent, (error) => {
+  fs.writeFile(result.localeData.filePath, fileContent, (error) => {
     if (error) {
       throw error;
     }
 
-    log(`File ${result.props.filePath} was successfully created.`)
+    log(`File ${result.localeData.filePath} was successfully created.`)
   })
 };
 
-const _get = (props, cb) => {
+const _get = (props) => {
   let url = `${SERVICE_PREFIX}/${props.locale.global}.${props.type}?key=${props.key}`;
 
   if (!!props['format']) {
@@ -41,16 +41,12 @@ const _get = (props, cb) => {
 
   log(`Getting i18n info for ${props.locale.global}.${props.type}`);
 
-  axios.get(url).then(response => {
-    const result = {
-      props,
-      data: response.data,
-    }
-    cb && cb(result);
-  });
+  return axios.get(url);
 };
 
 const get = (locales, types, pathPrefix) => {
+  let localesByType = [];
+
   locales.forEach(locale => {
     types.forEach(type => {
       const format = type === 'json' ? FORMAT : null;
@@ -64,15 +60,21 @@ const get = (locales, types, pathPrefix) => {
         key: KEY,
       };
 
-      _get(props, writeFile);
+      localesByType.push(props);
     });
   });
+
+  return Promise.all(localesByType.map(localeData => 
+    _get(localeData)
+      .then(response => writeFile({ localeData, data: response.data }))
+      .catch(err => console.log(err))
+  ));
 };
 
 const getAll = (pathPrefix) => {
   pathPrefix = pathPrefix || '.';
 
-  get(LOCALES, TYPES, pathPrefix);
+  return get(LOCALES, TYPES, pathPrefix);
 };
 
 module.exports = {
